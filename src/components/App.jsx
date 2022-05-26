e8adc694eb0a657dd46f1f423b5d11db073a11ca
eric-lazarus <eric@lazarus.enterprises>
2022-05-26 12:53:54 -0400

    Change file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,19 @@ import {storePayload} from '../actions/storePayload'
 /* Styles */
 import '../styles/App.css'
 
+const defaultGame = {
+  /* current level determines speed */
+  currentLevel: 1,
+  /* nextLevel is how many right answers player needs for next level */
+  nextLevel: 3,
+  /* Every level is faster by a factor of speedScaling */
+  speedScaling: 0.3,
+  /* player must beat x * levelScaling more levels to get to the next level */
+  levelScaling: 2,
+  /* Number of milliseconds it takes for wall to animate height of screen */
+  wallSpeed: 5000,
+}
+
 function App(props) {
   const playerRef = useRef()
   const [playerStats, _setPlayerStats] = useState({
@@ -23,25 +36,25 @@ function App(props) {
     width: 50,
     color: 'yellow',
   })
-  // const [playerPhysics, _setPlayerPhysics] = useState({
-  //   x: 0,
-  //   y: 0,
-  //   left: false,
-  //   right: false,
-  // })
+  const [playerPhysics, _setPlayerPhysics] = useState({
+    x: 0,
+    y: 0,
+    left: false,
+    right: false,
+  })
   const [mathProblem, _setMathProblem] = useState({
     problemString: '0 + 0 = ?',
     answer: 0,
     choices: [0, 0, 0, 0],
   })
   const [playerLane, _setPlayerLane] = useState(0)
-  const [wallSpeed, _setWallSpeed] = useState(props.defaultGame.wallSpeed)
-  const [nextLevel, _setNextLevel] = useState(props.defaultGame.nextLevel)
-  const [currentLevel, _setCurrentLevel] = useState(props.defaultGame.currentLevel)
+  const [wallSpeed, _setWallSpeed] = useState(defaultGame.wallSpeed)
+  const [nextLevel, _setNextLevel] = useState(defaultGame.nextLevel)
+  const [currentLevel, _setCurrentLevel] = useState(defaultGame.currentLevel)
   
   const currentLevelRef = useRef(currentLevel)
   const nextLevelRef = useRef(nextLevel)
-  const playerPhysicsRef = useRef(props.playerPhysics)
+  const playerPhysicsRef = useRef(playerPhysics)
   const gameplayContainerRef = useRef()
   const mathProblemRef = useRef(mathProblem)
   const playerStatsRef = useRef(playerStats)
@@ -60,9 +73,9 @@ function App(props) {
 
   useEffect(() => {
     if (playerStatsRef.current.right === nextLevelRef.current) {
-      tlRef.current.timeScale(1 + (currentLevelRef.current * props.defaultGame.speedScaling))
+      tlRef.current.timeScale(1 + (currentLevelRef.current * defaultGame.speedScaling))
       setCurrentLevel(currentLevelRef.current + 1)
-      setNextLevel(nextLevel * props.defaultGame.levelScaling)
+      setNextLevel(nextLevel * defaultGame.levelScaling)
     }
   }, [playerStats])
 
@@ -72,14 +85,14 @@ function App(props) {
       right: 0,
       wrong: 0,
     })
-    setCurrentLevel(props.defaultGame.currentLevel)
-    setNextLevel(props.defaultGame.nextLevel)
+    setCurrentLevel(defaultGame.currentLevel)
+    setNextLevel(defaultGame.nextLevel)
 
     document.addEventListener('keypress', onKeyPress)
     document.addEventListener('keyup', onKeyUp)
 
     tl = gsap.timeline({repeat: -1});
-    tl.timeScale(1 + ((currentLevelRef.current - 1) * props.defaultGame.speedScaling))
+    tl.timeScale(1 + ((currentLevelRef.current - 1) * defaultGame.speedScaling))
 
     /* part2Time is the time it takes the wall to go from top of player
        to bottom of gameplay container
@@ -145,35 +158,33 @@ function App(props) {
     tlRef.current = tl
   }
 
-  // useEffect(() => {
-  //   movePlayer()
-  // }, [playerPhysics])
+  useEffect(() => {
+    movePlayer()
+  }, [playerPhysics])
 
-  // function movePlayer() {
-  //   const isNotAtRightLimit =
-  //     playerPhysicsRef.current.x < gameplayContainerRef.current.offsetWidth - playerStyle.width
-  //   const isNotAtLeftLimit = playerPhysicsRef.current.x > 0
-  //   if (playerPhysics.right && isNotAtRightLimit) {
-  //     setPlayerPhysics({
-  //       ...playerPhysicsRef.current,
-  //       x: playerPhysicsRef.current.x + 1
-  //     })
-  //   } else if (playerPhysics.left && isNotAtLeftLimit) {
-  //     setPlayerPhysics({
-  //       ...playerPhysicsRef.current,
-  //       x: playerPhysicsRef.current.x - 1
-  //     })
-  //   }
-  // }
+  function movePlayer() {
+    const isNotAtRightLimit =
+      playerPhysicsRef.current.x < gameplayContainerRef.current.offsetWidth - playerStyle.width
+    const isNotAtLeftLimit = playerPhysicsRef.current.x > 0
+    if (playerPhysics.right && isNotAtRightLimit) {
+      setPlayerPhysics({
+        ...playerPhysicsRef.current,
+        x: playerPhysicsRef.current.x + 1
+      })
+    } else if (playerPhysics.left && isNotAtLeftLimit) {
+      setPlayerPhysics({
+        ...playerPhysicsRef.current,
+        x: playerPhysicsRef.current.x - 1
+      })
+    }
+  }
 
   /* handle state references */
 
   /*-----------------------------------*/
   const setPlayerPhysics = (data) => {
     playerPhysicsRef.current = data
-    props.storePayload({
-      playerPhysics: data,
-    })
+    _setPlayerPhysics(data)
   }
 
   const setMathProblem = (data) => {
@@ -302,8 +313,7 @@ function App(props) {
   }
 
   function getPlayerLane() {
-    console.log('game play container', gameplayContainerRef.current)
-    const gameplayContainerWidth = gameplayContainerRef.current ? gameplayContainerRef.current.offsetWidth : 0
+    const gameplayContainerWidth = gameplayContainerRef.current.offsetWidth
     const laneWidth = gameplayContainerWidth / 4
     if (playerPhysicsRef.current.x < laneWidth) {
       return 0
@@ -346,10 +356,6 @@ function App(props) {
         </div>
       </nav>
       <div className="gameplay-container" ref={gameplayContainerRef}>
-        <Player
-          gameplayContainerRef={gameplayContainerRef}
-          playerPhysicsRef={playerPhysicsRef}
-        />
         <div
           className="wall"
           ref={wallRef}
@@ -365,6 +371,20 @@ function App(props) {
             )
           })}
         </div>
+        <div
+          style={{
+            transform: `translate(${playerPhysics.x}px, 0)`,
+            width: `${playerStyle.width}px`,
+            height: `${playerStyle.height}px`,
+            // width: 0,
+            // height: 0,
+            // border: `${playerStyle.height / 2}px solid transparent`,
+            // borderTop: 0,
+            // borderBottom: `${playerStyle.height}px solid ${playerStyle.color}`,
+          }}
+          className="player"
+          ref={playerRef}
+        />
       </div>
     </div>
   )
